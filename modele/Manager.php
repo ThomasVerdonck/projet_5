ae674ba4d49f2ff5998572d1805b4f8393d3cad6
Thomas Verdonck <thomas_verdonck@yahoo.fr>
2021-01-21 13:32:19 +0100

    dernier ajustement

--- modele/Manager.php
+++ modele/Manager.php
@@ -7,9 +7,6 @@ class Manager
     protected function getBdd(){
         try{
             $bdd = new PDO('mysql:host=localhost:3308;dbname=projet5;charset=utf8', 'root', 'root');
-            $bdd->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-            $bdd->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
-
         }
         catch(Exception $e){
             die('Erreur : '.$e->getMessage());
